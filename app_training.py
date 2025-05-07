960803e6708ca856e61d258d3f70f2d1362f74fa
DangLamTung <danglamtung199@gmail.com>
2025-05-07 14:05:39 +0700

    Update app_training.py

--- app_training.py
+++ app_training.py
@@ -25,9 +25,7 @@ ADDRESS_DEFAULT = "[10.7724,106.65922]"
 
 
 # Set environment variables for API keys
-os.environ["SERPER_API_KEY"] = "b6ff922f76ebe3c41760ce93f6dfca48ee3cf3c6"
-os.environ["OPENROUTER_API_KEY"] = "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"
-os.environ["JINA_API_KEY"] = "jina_1255762ef83e40b9a7469eb8f4f4e146qaA4WxUxW8_E5Mpp6UcRF3yyeazS"
+
 
 search_agent = OpenDeepSearchTool(model_name="openrouter/google/gemini-2.5-pro-exp-03-25:free", reranker="jina") # Set reranker to "jina" or "infinity"
 model = LiteLLMModel(
